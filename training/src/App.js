609536167f31b1880f385f2668fb5703cfc065d3
arunk0035 <arunkumar.v@tcare.ai>
2023-04-11 16:07:43 +0530

    Training changes

--- training/src/App.js
+++ training/src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react"
 
 function App() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    fetch("https://newsapi.org/v2/top-headlines?country=in&apikey=2c29ba0b06414b688d5f88226993c08d&page=1")
+      .then((res) => res.json())
+      .then((response) => {
+        //console.log(response)
+        setData(response.articles)
+      })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          data.map((datas, id) =>
+            <div className='border'>Title :- {datas.title}</div>
+          )
+        }
       </header>
     </div>
   );
